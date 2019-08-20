e4c3329d306b37482f2b9d47491321ec3e2fe6da
VidhyaRavikumar <vidhyaswarnalatha@gmail.com>
2019-08-19 20:56:12 -0400

    modified functions

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ exports.config = {
       email: 'default',
       password: 'default',
       sendtoemail: 'default',
-      cmsub: 'test'
+      cmsub: 'Test Passed'
     }
   },
 };
--- specs/email.js
+++ specs/email.js
@@ -1,26 +1,36 @@
 describe('Gmail', function() {
-beforeEach(function(){
-browser.driver.ignoreSynchronization = true;
+    var B=browser.driver
+    beforeEach(function(){
+        B.ignoreSynchronization = true
+    });
+    afterEach(function() {
+        B.sleep(5000);
+    });
 
-});
-it('NavGmail', function() {
-browser.driver.get('http://gmail.com');
-browser.driver.findElement(by.id('identifierId')).sendKeys(browser.params.login.email);
-browser.driver.findElement(by.id('identifierNext')).click(); 
-browser.driver.sleep(5000);
-browser.driver.findElement(by.xpath("//input[@type='password']")).sendKeys(browser.params.login.password);
-browser.driver.findElement(by.id('passwordNext')).click(); 
-browser.driver.sleep(5000);
-browser.driver.get('https://mail.google.com/mail/u/0/#inbox?compose=new')
-browser.driver.sleep(5000);
-browser.driver.findElement(by.xpath("//textarea[@name='to']")).sendKeys(browser.params.login.sendtoemail);
-browser.driver.findElement(by.xpath("//input[@name='subjectbox']")).sendKeys(browser.params.login.cmsub);
-browser.driver.sleep(5000);
-browser.driver.findElement(by.xpath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")).click();
-browser.driver.get('https://mail.google.com/mail/u/0/#sent')
-browser.driver.sleep(5000);
+    it('emailid', function() {
+        B.get('http://gmail.com');
+        B.findElement(by.name('identifier')).sendKeys(browser.params.login.email);
+        B.findElement(by.id('identifierNext')).click(); 
+        });
 
-});
+    it('passwd', function() {
+        B.findElement(by.xpath("//input[@type='password']")).sendKeys(browser.params.login.password);
+        B.findElement(by.id('passwordNext')).click(); 
+        });
+
+    it('composeurl', function() {
+        B.get('https://mail.google.com/mail/u/0/#inbox?compose=new')
+        });
+
+    it('composemail', function() {
+        B.findElement(by.xpath("//textarea[@name='to']")).sendKeys(browser.params.login.sendtoemail);
+        B.findElement(by.xpath("//input[@name='subjectbox']")).sendKeys(browser.params.login.cmsub);
+        });
+
+    it('send_sent', function() {
+        B.findElement(by.xpath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")).click();
+        B.get('https://mail.google.com/mail/u/0/#sent')
+        });
 });
 
 
